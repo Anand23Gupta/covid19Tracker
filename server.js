e513df5b2d3769b76a3dfdcf990c23853e20d1b0
Anand.Gupta <anand.gupta@mitie.com>
2020-06-15 11:12:50 +0530

    server.js file modified

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/corona-tracker'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/corona-tracker/index.html'));});
 app.listen(process.env.PORT || 8080);
